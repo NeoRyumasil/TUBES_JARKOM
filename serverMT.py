ff3e43018a738a2cfa6fbda29070330d8b8fa9b1
Nazriel05 <arielkeren05@gmail.com>
2025-06-01 22:25:59 +0700

    Update serverMT.py

--- serverMT.py
+++ serverMT.py
@@ -1,4 +1,5 @@
 import socket
+import os
 import threading
 
 # Server configuration
@@ -7,20 +8,41 @@ PORT = 6789
 
 # Handle client connections
 def handle_client(client_socket, client_address):
-    print(f"Connection established with {client_address}")
-    try:
-        while True:
-            message = client_socket.recv(1024).decode('utf-8')
-            if not message:
-                break
-            print(f"Received from {client_address}: {message}")
-            client_socket.send(f"Echo: {message}".encode('utf-8'))
-    except ConnectionResetError:
-        print(f"Connection lost with {client_address}")
-    finally:
-        client_socket.close()
-        print(f"Connection closed with {client_address}")
+    request = client_socket.recv(1024).decode()
+    print(f"Request received from {client_address}:\n{request}")
 
+    lines = request.splitlines()
+    if  len(lines) == 0:
+        client_socket.close()
+        return
+
+    filename = lines[0].split()[1].lstrip('/')
+    if filename == '':
+        filename = 'index.html'
+    if os.path.isfile(filename):
+        with open(filename, 'rb') as f:
+            body = f.read() 
+        header = (
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            f"Content-Length: {len(body)}\r\n\r\n"
+        )   
+        response = header.encode() + body
+    else:
+        body = """
+            <html>
+                <head><title>404 Not Found</title></head>
+                <body>
+                    <h1>404 Not Found</h1>
+                    <p>File not found</p>
+                </body>
+            </html>
+        """
+        client_socket.sendall(
+            f"HTTP/1.1 404 Not Found\r\n"
+            "Content-Type: text/html\r\n"
+            f"Content-Length: {len(body)}\r\n\r\n".encode() + body.encode()
+        )
 # Main server function
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
